ce6f5d76d870788fcde25cb00cac701cf3f9ca2b
pablo <pablords@gmail.com>
2021-10-17 17:45:30 -0300

    *

--- configure.sh
+++ configure.sh
@@ -9,6 +9,7 @@ fi
 cp .env.example .env
 
 echo export CONFIG_PROD_HOME=~/config-server-production >> ~/.bashrc
+echo export JEKINS_HOME=~/config-server-production >> ~/.bashrc
 
 sudo setfacl -R -d -m o::rwx $JEKINS_HOME
 
